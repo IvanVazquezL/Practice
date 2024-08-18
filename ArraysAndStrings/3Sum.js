d0f99a6b6701cd451f9801eee17bc2d1a8382ea6
IvanVazquezL <ivanvl_11@hotmail.com>
2024-08-18 09:50:33 -0600

    (feat): twosumtwo added

---
+++ ArraysAndStrings/3Sum.js
@@ -0,0 +1,7 @@
+var threeSum = function(nums) {
+    
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([0,1,1]));
+console.log(threeSum([0,0,0]));
---
+++ ArraysAndStrings/TwoSumTwo.js
@@ -0,0 +1,25 @@
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [
+                left + 1,
+                right + 1
+            ]
+        }
+
+        if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+};
+
+console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,3,4], 6));
+console.log(twoSum([-1,0], -1));
