0f14b321a033a4a379eaecbabc9e5a386ef4f3ca
IvanVazquezL <ivanvl_11@hotmail.com>
2024-08-21 22:30:19 -0600

    (feat): missing number added

---
+++ missingNumber.js
@@ -0,0 +1,11 @@
+var missingNumber = function(nums) {
+    const set = new Set(nums);
+
+    for (let i = 0; i <= nums.length; i++) {
+        if (!set.has(i)) return i;
+    }
+};
+
+console.log(missingNumber([3,0,1]));
+console.log(missingNumber([0,1]));
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]));
